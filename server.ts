91c2853f625c376fbc37b7e6c24cea745d97c3e6
roycehoe <royceinbox@hotmail.com>
2022-01-26 23:30:08 +0800

    Implement get method on backend server that gets all data from database

--- server.ts
+++ server.ts
@@ -6,14 +6,21 @@ import { get } from "./database";
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, { cors: { origin: "*" } });
+const recordRoutes = express.Router()
 
-app.get('/', (req: any, res) => {
-  res.send("hello")
+app.get('/', async (req: any, res) => {
+  const data = await get()
+  res.json(data)
 })
 
-io.on("connection", (socket: any) => {
-  // ...
-});
+
+// app.get('/', (req: any, res) => {
+//   res.send("hello")
+// })
+
+// io.on("connection", (socket: any) => {
+//   // ...
+// });
 
 httpServer.listen(3000, () => {
   console.log('server runing')
