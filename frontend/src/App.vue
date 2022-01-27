c04adfc07124b2a4c6d28071711cb40e77bcd46a
roycehoe <royceinbox@hotmail.com>
2022-01-28 01:10:02 +0800

    Implement tailwind and daisyui components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,51 +1,7 @@
-<!-- <script setup lang="ts">
-import { io } from "socket.io-client"
-import { onBeforeMount, ref } from "vue";
-import { MessageDisplay, useMessage } from "./composables/useMessage";
-import { CreateMessageRequest } from "./services/createMessageRequest";
-
-const { messageHistory, createMessageResponse, getAllMessage, createMessage } = useMessage()
-const messageSessionHistory = ref([] as Array<MessageDisplay>)
-const messageForm = ref({} as SubmitMessageForm)
-
-const socket = io("http://localhost:8000")
-
-interface SubmitMessageForm extends CreateMessageRequest {
-}
-
-async function sendMessage(messageForm: SubmitMessageForm) {
-  await createMessage(messageForm)
-  socket.emit('message', createMessageResponse.value)
-}
-
-async function setupMessageSocket() {
-  socket.on('message', (data) => {
-    messageSessionHistory.value.push(data)
-  })
-}
-
-
-onBeforeMount(() => { setupMessageSocket(); getAllMessage() })
-
-</script>
-
-<template>
-  <p>Create Message Response: {{ createMessageResponse }}</p>
-  <form @submit.prevent="sendMessage(messageForm)">
-    <input type="text" v-model="messageForm.username" />
-    <input type="text" v-model="messageForm.message" />
-    <button>Send message</button>
-  </form>
-  <p>message form: {{ messageForm }}</p>
-  <p>Broadcasted message: {{ messageSessionHistory }}</p>
-  <p>Message database: {{ messageHistory }}</p>
-</template>
-
-<style>
-</style> -->
-
 <script setup lang="ts">
 import Chatroom from './components/Chatroom.vue';
+
+
 </script>
 
 <template>
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { io } from "socket.io-client"
+import { onBeforeMount, ref } from "vue";
+import { MessageDisplay, messageForm, SubmitMessageForm, useMessage } from "../composables/useMessage";
+
+const { messageHistory, createMessageResponse, getAllMessage, createMessage } = useMessage()
+const messageSessionHistory = ref([] as Array<MessageDisplay>)
+
+const socket = io("http://localhost:8000")
+
+async function sendMessage(messageForm: SubmitMessageForm) {
+    await createMessage(messageForm)
+    socket.emit('message', createMessageResponse.value)
+}
+
+async function setupMessageSocket() {
+    socket.on('message', (data) => {
+        messageSessionHistory.value.push(data)
+    })
+}
+
+
+onBeforeMount(() => { setupMessageSocket(); getAllMessage() })
+
+</script>
+
+<template>
+    <p>Create Message Response: {{ createMessageResponse }}</p>
+    <form @submit.prevent="sendMessage(messageForm)">
+        <input type="text" v-model="messageForm.username" />
+        <input type="text" v-model="messageForm.message" />
+        <button>Send message</button>
+    </form>
+    <p>message form: {{ messageForm }}</p>
+    <p>Broadcasted message: {{ messageSessionHistory }}</p>
+    <p>Message database: {{ messageHistory }}</p>
+</template>
+
+<style>
+</style>
