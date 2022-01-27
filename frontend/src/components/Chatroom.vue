62dd953f281624e4468ec18e0532c83045d29c13
roycehoe <royceinbox@hotmail.com>
2022-01-28 03:28:00 +0800

    Implement writing and submitting of messages in chatroom

--- frontend/src/components/Chatroom.vue
+++ frontend/src/components/Chatroom.vue
@@ -4,20 +4,20 @@ import { onBeforeMount, ref } from "vue";
 import { isJoinedChat, MessageDisplay, messageForm, messageHistory, SubmitMessageForm, useMessage } from "../composables/useMessage";
 
 
-const { createMessageResponse, getAllMessage, createMessage } = useMessage()
+const { createMessageResponse, createMessage } = useMessage()
 const messageSessionHistory = ref([] as Array<MessageDisplay>)
 
 const socket = io("http://localhost:8000")
 
-async function sendMessage(messageForm: SubmitMessageForm) {
-    await createMessage(messageForm)
-    socket.emit('message', createMessageResponse.value)
+async function sendMessage() {
+  await createMessage(messageForm.value)
+  socket.emit('message', createMessageResponse.value)
 }
 
 async function setupMessageSocket() {
-    socket.on('message', (data) => {
-        messageSessionHistory.value.push(data)
-    })
+  socket.on('message', (data) => {
+    messageSessionHistory.value.push(data)
+  })
 }
 
 
@@ -26,16 +26,32 @@ onBeforeMount(() => { setupMessageSocket() })
 </script>
 
 <template>
-    <div v-if="isJoinedChat">
-        <form @submit.prevent="sendMessage(messageForm)">
-            <input type="text" v-model="messageForm.username" />
-            <input type="text" v-model="messageForm.message" />
-            <button>Send message</button>
+  <div v-if="isJoinedChat">
+    <div class="hero min-h-screen bg-base-200">
+      <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div class="card-body">
+          <p>Messages go here</p>
+          <p>Messages go here</p>
+          <p>Messages go here</p>
+          <p>Messages go here</p>
+          <p>Messages go here</p>
+        </div>
+        <form @submit.prevent="sendMessage">
+          <div class="card bg-base-200 rounded-none">
+            <input
+              v-model="messageForm.message"
+              type="text"
+              placeholder="Write a message..."
+              class="input bg-base-200"
+            />
+          </div>
         </form>
-        <p>message form: {{ messageForm }}</p>
-        <p>Broadcasted message: {{ messageSessionHistory }}</p>
-        <p>Message database: {{ messageHistory }}</p>
+      </div>
     </div>
+    <p>message form: {{ messageForm }}</p>
+    <p>Broadcasted message: {{ messageSessionHistory }}</p>
+    <p>Message database: {{ messageHistory }}</p>
+  </div>
 </template>
 
 <style>
