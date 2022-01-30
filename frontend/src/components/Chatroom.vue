8b2c469e7f52eab0ea44655be0d39b2de8e3aff7
roycehoe <royceinbox@hotmail.com>
2022-01-30 11:54:40 +0800

    Implement better looking chat menu; To implement better looking scrollbar next

--- frontend/src/components/Chatroom.vue
+++ frontend/src/components/Chatroom.vue
@@ -20,14 +20,16 @@ async function sendMessage() {
 
 <template>
   <div v-if="isJoinedChat">
-    <div class="min-h-screen min-w-full bg-base-200 flex justify-center">
-      <div class="card flex-shrink-0 w-full max-w-2xl my-28 shadow-2xl bg-base-100">
+    <!-- <div class="bg-base-200 flex justify-center">
+      <div
+        class="card flex-shrink-0 max-w-2xl my-28 shadow-2xl bg-base-100 overflow-y-scroll w-4/5 block h-4/5"
+      >
         <div class="card-body">
           <Message></Message>
         </div>
       </div>
-    </div>
-    <form @submit.prevent="sendMessage" class="sticky bottom-0 bg-green-900">
+    </div>-->
+    <!-- <form @submit.prevent="sendMessage" class="sticky bottom-0 bg-green-900">
       <div class="card bg-base-100 rounded-none">
         <input
           v-model="messageForm.message"
@@ -36,7 +38,26 @@ async function sendMessage() {
           class="input bg-base-100"
         />
       </div>
-    </form>
+    </form>-->
+    <div class="bg-base-200 flex justify-center min-h-screen h-screen flex-col items-center">
+      <div
+        class="card flex-shrink-0 max-w-4xl mt-28 shadow-2xl bg-base-100 overflow-auto w-4/5 block h-4/5"
+      >
+        <div class="card-body">
+          <Message></Message>
+        </div>
+      </div>
+      <form @submit.prevent="sendMessage" class="w-full max-w-4xl mb-28 mt-2">
+        <div class="card bg-base-100 rounded-none">
+          <input
+            v-model="messageForm.message"
+            type="text"
+            placeholder="Write a message..."
+            class="input bg-base-100"
+          />
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
