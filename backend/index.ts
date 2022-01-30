e7aba33db9219317693d39d4338320f55b10f883
roycehoe <royceinbox@hotmail.com>
2022-01-30 08:26:19 +0800

    Refactor index.ts file in backend

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { getMessage } from "./database";
+import * as core from 'express-serve-static-core'
+
 
 var router = require('./router')
 var cors = require('cors')
@@ -12,20 +14,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", router)
 
-const httpServer = createServer(app);
-httpServer.listen(8000);
 
-const io = new Server(httpServer, { cors: { origin: "*" } });
+function createSocketServer(app: core.Express) {
+  const httpServer = createServer(app);
+  httpServer.listen(8000);
+  const io = new Server(httpServer, { cors: { origin: "*" } })
+
+  io.on("connect_error", (err) => {
+    console.log(`connect_error due to ${err.message}`);
+  });
 
 
-io.on("connect_error", (err) => {
-  console.log(`connect_error due to ${err.message}`);
-});
+  io.on("connection", (socket) => {
+    console.log("connected")
 
 
-io.on("connection", (socket) => {
-  console.log("connected")
-  socket.on("message", (data) => { //listen for message event
-    io.emit('message', data) //emit message to everyone
+    socket.on("message", (data) => { //listen for message event
+      io.emit('message', data) //emit message to everyone
+    })
   })
-})
+}
+
+createSocketServer(app)
