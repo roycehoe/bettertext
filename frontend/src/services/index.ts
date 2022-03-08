a614c32bf6ffd39d8baf3e27fba0f4b14094f218
roycehoe <royceinbox@hotmail.com>
2022-03-08 13:40:34 +0800

    Save point; Tweaking reverse proxy settings, revert upon failure

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -2,6 +2,7 @@ import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
 // const BASEURL = "https://bettertext.fancybinary.sg/api"
 export const BASEURL = "http://localhost/api"
+// export const BASEURL = "http://localhost:8000"
 
 
 export const client: AxiosInstance = axios.create({
