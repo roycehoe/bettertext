f918acd958d2fce145aa9c76b832468219733e0d
roycehoe <royceinbox@hotmail.com>
2022-03-08 11:24:46 +0800

    Implement partially working reverse proxy, frontend and backend; socket not working; to debug problem

--- backend/config.ts
+++ backend/config.ts
@@ -1,4 +1,5 @@
 // export const DB_URL = 'mongodb://localhost:27017'
 export const DB_URL = 'mongodb://database'
 export const DB_NAME = 'mongochat'
-export const SOCKET_IO_PORT = 8000
+// export const SOCKET_IO_PORT = 8000
+export const SOCKET_IO_PORT = 80
--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
 // const BASEURL = "https://bettertext.fancybinary.sg/api"
-export const BASEURL = "http://localhost:8000"
+export const BASEURL = "http://localhost/api"
 
 
 export const client: AxiosInstance = axios.create({
